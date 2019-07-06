2a745b60fcf07ac376afebee9fc529265641709f
1160300127 <3411320375@qq.com>
2019-07-06 12:57:41 +0800

    edited helloworld

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world!");
-                                System.out.println("I really don't know how to change it, so I add a sentence here!");
+        System.out.println("I really don't know how to change it, so I add a sentence here!");
 	}
 
 }
